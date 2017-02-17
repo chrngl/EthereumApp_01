983fac0589d6a842cfad5c4354fe63b25b8ae47f
Chris <chrisenspam@gmail.com>
2017-02-17 14:34:45 +0100

    Initial commit

---
+++ app/src/main/java/vcPattern/viewController.java
@@ -0,0 +1,23 @@
+package vcPattern;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.blockchainthis.ethereumpreisabfrage.R;
+
+/**
+ * Created by Chris on 16.02.2017.
+ */
+
+public class viewController extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView priceTV;
+        priceTV = (TextView) findViewById(R.id.priceTV);
+
+}
+
